a77e3bca4bfe4095ba9bc841e39f9a32406a5f76
josuesequeira2298 <josue_2298_@outlook.com>
2017-02-01 17:51:33 -0600

    commit tarea1

---
+++ Tarea1/Tarea1/JavaScript.js
@@ -0,0 +1,5 @@
+﻿var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+btnMenu.addEventListener('click', function () {
+    nav.classList.toggle('mostrar');
+});
